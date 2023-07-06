1c912326944d8bf6cd0d69244f1aafb26151a5be
Andrey Partyanskiy <andreypartyanskiy@gmail.com>
2023-07-06 22:39:53 +0300

    first commit

---
+++ cppl-homeworks_02_1/cppl-homeworks_02_1/cppl-homeworks_02_1/cppl-homeworks_02_1.cpp
@@ -0,0 +1,28 @@
+﻿// cppl-homeworks_02_1.cpp 
+
+#include <iostream>
+using namespace std; 
+
+struct point
+{
+	int m_x;
+	int m_y;
+};
+
+void print_point(const point& point_object)
+{
+	cout << "x:" << point_object.m_x << ", y: "
+		<< point_object.m_y << endl;
+}
+
+int main()
+{
+	int i;
+	
+	for (i = 0; i < 5; i++) 
+	{
+		point my_point{ i,i*2 };
+		print_point(my_point);
+	}
+	return 0;
+}
---
+++ cppl-homeworks_02_2/cppl-homeworks_02_2/cppl-homeworks_02_2.cpp
@@ -0,0 +1,19 @@
+﻿// cppl-homeworks_02_2.cpp 
+
+#include <iostream>
+//#include <string>
+
+double abc(double f,double p) 
+{
+    double t = f / p;
+    return t;
+}
+
+int main()
+{
+    int a;//неиспользованная локальная переменная(синтаксическая)
+    //std::cout << abc() << std::endl; // abc: функция не принемает 0 аргументов (синтаксическая)
+    //std::cout << abc(5,0) << std::endl; // Делить на ноль нельзя (семантическая ошибка)
+    std::cout << std::to_string(abc(5, 2)) << std::endl;// "to_string": не является членом "std" Добавить библиотеку (ошибка линковки)
+    return 0;
+}
